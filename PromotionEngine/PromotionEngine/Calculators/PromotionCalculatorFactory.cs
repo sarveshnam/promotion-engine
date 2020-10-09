b45cc97dd38c99e44c0e1f19d02f64ab2f5de262
Sarvesh Namjoshi (MINDTREE LIMITED) <v-sanamj@microsoft.com>
2020-10-09 22:50:46 +0530

    Added GetPromotionCalculator to PromotionCalculatorFactory

--- PromotionEngine/PromotionEngine/Calculators/PromotionCalculatorFactory.cs
+++ PromotionEngine/PromotionEngine/Calculators/PromotionCalculatorFactory.cs
@@ -8,7 +8,32 @@ namespace PromotionEngine
     {
         public static PromotionCalculator GetPromotionCalculator(PromotionType type)
         {
-            return null; //TODO: Need to implement logic here
+            switch (type)
+            {
+                case PromotionType.AQuantity:
+                    IDictionary<SkuType, decimal> skuA = new Dictionary<SkuType, decimal>()
+                    {
+                        { SkuType.A, 3}
+                    };
+                    return new AQuantityCalculator(skuA, 130, true);
+
+                case PromotionType.BQuantity:
+                    IDictionary<SkuType, decimal> skuB = new Dictionary<SkuType, decimal>()
+                    {
+                        { SkuType.B, 2}
+                    };
+                    return new BQuantityCalculator(skuB, 45, true);
+
+                case PromotionType.CDQuantity:
+                    IDictionary<SkuType, decimal> skuCD = new Dictionary<SkuType, decimal>()
+                    {
+                        { SkuType.C, 1},
+                        { SkuType.D, 1}
+                    };
+                    return new CDQuantityCalculator(skuCD, 30, true);
+                default:
+                    return null;
+            }
         }
     }
 }
