88caacdc4c681c8a4f725eb081618df6c1a72716
Sarvesh Namjoshi (MINDTREE LIMITED) <v-sanamj@microsoft.com>
2020-10-09 22:13:58 +0530

    Added PromotionCalculatorFactory

---
+++ PromotionEngine/PromotionEngine/Calculators/PromotionCalculatorFactory.cs
@@ -0,0 +1,14 @@
+﻿using PromotionEngine.Calculators;
+using PromotionEngine.Entities;
+using System.Collections.Generic;
+
+namespace PromotionEngine
+{
+    class PromotionCalculatorFactory
+    {
+        public static PromotionCalculator GetPromotionCalculator(PromotionType type)
+        {
+            return null; //TODO: Need to implement logic here
+        }
+    }
+}
