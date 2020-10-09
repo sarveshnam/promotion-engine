76ba3661f4d0329c37337efb87ba5085749ae13f
Sarvesh Namjoshi (MINDTREE LIMITED) <v-sanamj@microsoft.com>
2020-10-09 22:21:16 +0530

    Added Calculate method in CDQuantityCalculator

--- PromotionEngine/PromotionEngine/Calculators/CDQuantityCalculator.cs
+++ PromotionEngine/PromotionEngine/Calculators/CDQuantityCalculator.cs
@@ -11,5 +11,15 @@ namespace PromotionEngine
         public CDQuantityCalculator(IDictionary<SkuType, decimal> sku, decimal price, bool isActive) : base(sku, price, isActive)
         {
         }
+
+        public override decimal Calculate(IList<OrderLine> orderLines)
+        {
+            decimal total = 0m;
+            if (orderLines == null || orderLines.Count == 0)
+                return 0m;
+
+            //TODO: Need to add rest of the logic here
+            return total;
+        }
     }
 }
