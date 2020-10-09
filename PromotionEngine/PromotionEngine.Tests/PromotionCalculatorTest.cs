d25c4b2a2fb14c92b5c509721194eee839630636
Sarvesh Namjoshi (MINDTREE LIMITED) <v-sanamj@microsoft.com>
2020-10-09 10:26:32 +0530

    Added test project

---
+++ PromotionEngine/PromotionEngine.Tests/PromotionCalculatorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PromotionEngine.Tests
+{
+    [TestClass]
+    public class PromotionCalculatorTest
+    {
+        [TestMethod]
+        public void ScenarioATest()
+        {
+
+        }
+
+        [TestMethod]
+        public void ScenarioBTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void ScenarioCTest()
+        {
+
+        }
+    }
+}
---
+++ PromotionEngine/PromotionEngine.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PromotionEngine.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PromotionEngine.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cdab7313-1f42-4e13-a8d3-c592e536a92f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
