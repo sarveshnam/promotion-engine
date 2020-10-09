dec93e231779059f0f759fca5cd189a45c52cd5d
Sarvesh Namjoshi (MINDTREE LIMITED) <v-sanamj@microsoft.com>
2020-10-09 22:06:29 +0530

    Calculate method of PromotionCalculator are applicable for Promotion A and B

--- PromotionEngine/PromotionEngine/Calculators/AQuantityCalculator.cs
+++ PromotionEngine/PromotionEngine/Calculators/AQuantityCalculator.cs
@@ -10,5 +10,11 @@ namespace PromotionEngine.Calculators
         public AQuantityCalculator(IDictionary<SkuType, decimal> sku, decimal price, bool isActive) : base(sku, price, isActive)
         {
         }
+
+        //The promotion calculation defined in PromotionCalculator is applicable here
+        //If the implementation changes in future, override the Calculate method 
+        //public override decimal Calculate(IList<OrderLine> orderLines)
+        //{
+        //}
     }
 }
--- PromotionEngine/PromotionEngine/Calculators/BQuantityCalculator.cs
+++ PromotionEngine/PromotionEngine/Calculators/BQuantityCalculator.cs
@@ -11,5 +11,11 @@ namespace PromotionEngine
         public BQuantityCalculator(IDictionary<SkuType, decimal> sku, decimal price, bool isActive): base(sku, price, isActive)
         {
         }
+
+        //The promotion calculation defined in PromotionCalculator is applicable here
+        //If the implementation changes in future, override the Calculate method
+        //public override decimal Calculate(IList<OrderLine> orderLines)
+        //{
+        //}
     }
 }
